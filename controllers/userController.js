9f4756a24680bcf54f155b8a7b5061cb087e3288
SilverGar <a00829450@itesm.mx>
2022-05-26 16:59:07 -0500

    Admin users finished

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,31 +4,31 @@ const csv = require('csv-parser')
 
 
 
-class MainController{
-    async getUserType(req, res){
-        try{
+class MainController {
+    async getUserType(req, res) {
+        try {
             const pool = await poolPromise
             const result = await pool.request()
-            .input('email', sql.VarChar, req.params.email)
-            .query("EXECUTE GetUserType @email")
+                .input('email', sql.VarChar, req.params.email)
+                .query("EXECUTE GetUserType @email")
             res.json(result.recordset)
         }
-        catch(error){
+        catch (error) {
             res.status(500)
             res.send(error.message)
         }
     }
 
-    async updateUserType(req, res){
-        try{
+    async updateUserType(req, res) {
+        try {
             const pool = await poolPromise
             const result = await pool.request()
-            .input('type', sql.Int, req.params.type)
-            .input('email', sql.VarChar, req.params.email)
-            .query("EXECUTE UpdateUserType @type, @email")
+                .input('type', sql.Int, req.params.type)
+                .input('email', sql.VarChar, req.params.email)
+                .query("EXECUTE UpdateUserType @type, @email")
             res.json(result)
         }
-        catch(error){
+        catch (error) {
             res.status(500)
             res.send(error.message)
         }
@@ -38,7 +38,7 @@ class MainController{
         try {
             const pool = await poolPromise
             const result = await pool.request()
-            .query("EXECUTE SU_getUsers")
+                .query("EXECUTE SU_getUsers")
             res.json(result.recordset)
         }
         catch (error) {
@@ -47,48 +47,81 @@ class MainController{
         }
     }
 
-    async getUpdate(req, res){
+    async getUpdate(req, res) {
         try {
             const pool = await poolPromise
             const result = await pool.request()
-            .input('email', sql.VarChar, req.params.email)
-            .query("EXECUTE EM_getUpdate @email")
+                .input('email', sql.VarChar, req.params.email)
+                .query("EXECUTE EM_getUpdate @email")
             res.json(result.recordset[0].updateTeam)
         }
-        catch(error){
+        catch (error) {
             res.status(500)
             res.send(error)
         }
     }
 
-    async receivedUpdate(req, res){
+    async receivedUpdate(req, res) {
         try {
             const pool = await poolPromise
             const result = await pool.request()
-            .input('email', sql.VarChar, req.params.email)
-            .query("EXECUTE EM_updateUpdate @email")
-            
+                .input('email', sql.VarChar, req.params.email)
+                .query("EXECUTE EM_updateUpdate @email")
+
             res.json(result.recordset)
         }
-        catch(error){
+        catch (error) {
             res.status(500)
             res.send(error)
         }
     }
 
-    async getReleasedStatus(req, res){
-        try{
+    async getReleasedStatus(req, res) {
+        try {
             const pool = await poolPromise
             const result = await pool.request()
-            .query("EXECUTE getReleasedStatus")
+                .query("EXECUTE getReleasedStatus")
             res.json(result.recordset[0].Status)
         }
-        catch(error){
+        catch (error) {
             res.status(500)
             res.send(error)
         }
     }
 
+    async addUser(req, res) {
+        try {
+            const pool = await poolPromise
+            const result = await pool.request()
+                .input('name', sql.VarChar, req.body.Name)
+                .input('email', sql.VarChar, req.body.Email)
+                .input('ishr', sql.Int, req.body.IsHR)
+                .query("EXECUTE SU_AddNewUser @name, @email, @ishr")
+            res.json(result.recordset)
+        } catch (error) {
+            res.status(500)
+            res.send(error)
+        }
+    }
+
+    async changeUserType(req, res) {
+        console.log(req.body);
+        console.log('hola');
+        try {
+
+            for (var i in req.body) {
+                const pool = await poolPromise
+                const result = await pool.request()
+                    .input('id', sql.Int, req.body[i].id)
+                    .input('status', sql.Int, req.body[i].SU_decision)
+                    .query("EXECUTE SU_UpdateUserLevel @id, @status")
+                res.json(result.recordser)
+            }
+        } catch (error) {
+            res.status(500)
+            res.send(error)
+        }
+    }
 }
 
 const userController = new MainController()
